b7a0f47054ed9ab67e8891e9bf5eafc020138b01
helico-kalki <schleimiggg@hotmail.com>
2025-08-28 11:25:20 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,128 @@
+from PyQt6.QtCore import Qt
+# ✨ - * + KALKI CONFIG + * - ✨
+
+# 🧩 SHORTCUTS
+#--------------------------------
+
+# 🦺 Toolbar
+
+CLEAR_CANV="Ctrl+N"
+IMP="Ctrl+O"
+EXP="Ctrl+S"
+UNDO="Ctrl+Z"
+REDO="Ctrl+Y"
+COPY="Ctrl+C"
+PASTE="Ctrl+V"
+ZOOM_IN="Ctrl++"
+ZOOM_OUT="Ctrl+-"
+RESIZE="Ctrl+R"
+CROPTOSEL="Ctrl+Shift+X"
+ROTATE90_CW="Ctrl+H"
+ROTATE90_CCW="Ctrl+G"
+MIRROR_H="Ctrl+Shift+H"
+MIRROR_V="Ctrl+Shift+V"
+
+
+# 🎆 Effects
+
+CA="Ctrl+Shift+C"
+GB="Ctrl+Shift+B"
+SMOOTH="Ctrl+Shift+W"
+SMOOTHMORE="Ctrl+Alt+W"
+UNSHARP="Ctrl+Shift+U"
+SHARPEN="Ctrl+Shift+Q"
+CONTOUR="Ctrl+ALt+C"
+FINDEDGES="Ctrl+Shift+E"
+EDGEENHANCE="Ctrl+Alt+E"
+EMBOSS="Ctrl+Shift+R"
+DETAIL="Ctrl+Shift+D"
+INVERT="Ctrl+Shift+I"
+
+# 🥏 Taskbar
+
+RSEL="S"
+CSEL="Ctrl+E"
+LSEL="Ctrl+L"
+DELSEL="Del"
+MOVE="M"
+
+RECT="R"
+ELLIPSE="E"
+TRIANGLE="Ctrl+T"
+LINES="L"
+DISABLESHAPE="Ctrl+Del"
+TEXT="T"
+
+DRAW="D"
+PEN="P"
+MARKER="Ctrl+P"
+BRUSH="B"
+ERASER="X"
+
+COLORPICKER="K"
+COLOR="C"
+BUCKET="F"
+GRADIENT="G"
+
+# 🔮 DEFAULT VALUES
+#-------------------------------
+
+# 🧧 Window
+WINTITLE="Kalki"
+WINSIZE=(0, 0, 1000, 1000)
+WINICON="colored_textures/logo.png"
+WINCOLOR="lightgray"
+WARNBFCLOSE=True
+
+# 🔲 Canvas
+CANVSIZE=(900, 900)
+CANVCOLOR=Qt.GlobalColor.white
+WARNBFCLEAR=True
+
+# 📆 Font
+FONT="Lexend Deca" # won't change the font in menus (sadly)
+
+# 🎹 Default Text Settings
+TEXTSIZE=32
+TEXTCOLOR="black"
+TEXTRANGE=(8,128)
+TEXT_BOLD=False
+TEXT_ITALIC=False
+TEXT_UNDERLINE=False
+TEXT_STRIKEOUT=False
+
+# 🧿 Selection
+SELCOLOR="#00C0FD"
+SELWIDTH=2
+SELVCOLOR="#001121"
+SELVSIZE=5
+SELVWIDTH=8
+SELSTYLE=Qt.PenStyle.DashLine
+
+# Drawing
+DWIDTHRANGE=(1,150)
+# ⚫ Pen
+PENCOLOR="black"
+PENWIDTH=2
+PENCAP=Qt.PenCapStyle.RoundCap
+PENJOIN=Qt.PenJoinStyle.RoundJoin
+PENSTYLE=Qt.PenStyle.SolidLine
+# 🟨 Marker
+MARKERCOLOR=(255,255,0,100)
+MARKERWIDTH=20
+MARKERCAP=Qt.PenCapStyle.FlatCap
+MARKERJOIN=Qt.PenJoinStyle.MiterJoin
+# 💙 Brush
+BRUSHCOLOR="#55aaff"
+BRUSHWIDTH=10
+BRUSHCAP=Qt.PenCapStyle.RoundCap
+BRUSHJOIN=Qt.PenJoinStyle.RoundJoin
+# ⚪ Eraser
+ERASERCOLOR="white"
+ERASERWIDTH=30
+ERASERCAP=Qt.PenCapStyle.RoundCap
+ERASERJOIN=Qt.PenJoinStyle.RoundJoin
+
+
+
+
